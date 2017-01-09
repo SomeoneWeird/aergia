55dd54605facef2196f5d4b7d785ae220f31f019
Curia <admin@curiadesign.net>
2017-01-09 22:59:41 +1100

    Section navigation added, some styling variables

--- app/containers/home/index.js
+++ app/containers/home/index.js
@@ -3,17 +3,21 @@ import React, { Component } from 'react';
 import { Link } from 'react-router';
 
 import section from '../cssModules/section.scss';
+import content from '../cssModules/content.scss';
 
 export default class HomePage extends Component {
   render() {
     return (
-        <section>
-            <h2 className={section.title}>3DS Hacks</h2>
-            <div className={section.content}>
-              <p>An interactive guide to A9LH + Luma on your 3DS</p>
-            </div>
-            <Link className={section.button} to='/config/model'>Start</Link>
-        </section>
+      <section>
+        <h2 className={section.title}>3DS Hacks</h2>
+        <div className={section.content}>
+          <p>An interactive guide to A9LH + Luma on your 3DS</p>
+        </div>
+        <div className={section.navigation}>
+          <Link className={content.button} to="/config/model">Back</Link>
+          <Link className={content.button} to="/config/model">Start</Link>
+        </div>
+      </section>
     );
   }
 }
