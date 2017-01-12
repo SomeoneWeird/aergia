0a9555fb137543bdf7646346c2c2817e2e4cdd6b
Adam Brady <adam@boxxen.org>
2017-01-12 23:31:24 +1100

    refactor soundhax screen

--- app/containers/entrypoints/soundhax/index.js
+++ app/containers/entrypoints/soundhax/index.js
@@ -5,7 +5,7 @@ import fs from 'fs'
 import { ipcRenderer } from 'electron';
 import React, { Component } from 'react';
 import { Link, browserHistory } from 'react-router';
-import { Circle } from 'rc-progress'
+import { ScaleLoader } from 'halogen'
 
 import request from 'request'
 import requestProgress from 'request-progress'
@@ -21,180 +21,138 @@ import content from '../../cssModules/content.scss'
 
 import styles from './index.css'
 
-const downloadTo = os.tmpdir()
-
-let items = {
-  soundhax: {
-    url: null, // set at runtime
-    filename: 'soundhax.m4a'
-  },
-  hbsk: {
-    url: 'http://smealum.github.io/ninjhax2/starter.zip',
-    filename: 'starter.zip',
-    extract: true,
-    extractFolderRoot: 'starter'
-  }
-}
-
 let SoundHax = React.createClass({
   getInitialState() {
     return {
-      downloads: {
-        soundhax: 0,
-        hbsk: 0,
-        otherapp: 0
-      },
-      copy: {
-        soundhax: 0,
-        hbsk: 0,
-        otherapp: 0
-      }
+      text: '',
+      downloading: null,
+      progress: 0,
+      finished: false
     }
   },
   componentDidMount() {
-    this.startDownload()
+    this.downloadSoundhax()
   },
-  getDownloadProgress(dl) {
-    return this.state.downloads[dl]
-  },
-  getCopyProgress(dl) {
-    return this.state.copy[dl]
-  },
-  download(item) {
-    console.log('downloading', item)
-    requestProgress(request(items[item].url))
+  downloadFile (filename, url, done) {
+    this.setState({
+      ...this.state,
+      progress: 0
+    })
+    console.log('downloading to', path.resolve(config.drive.mountPoint, filename))
+    requestProgress(request(url))
       .on('progress', (state) => {
         this.setState({
           ...this.state,
-          downloads: {
-            ...this.state.downloads,
-            [item]: (state.percent * 100).toFixed(2)
-          }
+          progress: (state.percent * 100).toFixed(0)
         })
       })
       .on('end', (err) => {
         if (err) {
-          console.error(err)
+          return done(err)
         }
         this.setState({
           ...this.state,
-          downloads: {
-            ...this.state.downloads,
-            [item]: 100
-          }
+          progress: 100
         })
-        this.handleComplete(item)
+        return done()
       })
-      .pipe(fs.createWriteStream(path.resolve(downloadTo, items[item].filename)))
+      .pipe(fs.createWriteStream(path.resolve(config.drive.mountPoint, filename)))
   },
-  handleComplete(item) {
-    let itemConfig = items[item]
+  downloadSoundhax () {
+    this.setState({
+      ...this.state,
+      downloading: 'Soundhax'
+    })
+    const url = `https://github.com/nedwill/soundhax/blob/master/soundhax-${config.region.toLowerCase()}-${config.model}.m4a?raw=true`
+    this.downloadFile('soundhax.m4a', url, (err) => {
+      if (err) {
+        return console.error('Error downloading soundhax...', err)
+      }
+      this.downloadOtherapp()
+    })
+  },
+  downloadOtherapp () {
+    this.setState({
+      ...this.state,
+      downloading: 'otherapp.bin'
+    })
+    ipcRenderer.on('otherappURLReply', (event, url) => {
+      this.downloadFile('otherapp.bin', url, (err) => {
+        if (err) {
+          return console.error('Error downloading otherapp.bin...', err)
+        }
+        this.downloadHBSK()
+      })
+    })
+    ipcRenderer.send('otherappURL', JSON.stringify(config))
+  },
+  downloadHBSK () {
+    this.setState({
+      ...this.state,
+      downloading: 'Homebrew Starter Kit'
+    })
+    this.downloadFile('starter.zip', 'http://smealum.github.io/ninjhax2/starter.zip', (err) => {
+      if (err) {
+        return console.error('Error downloading starter.zip...', err)
+      }
 
-    if (itemConfig.extract) {
-      let zip = new admZip(path.resolve(downloadTo, itemConfig.filename))
+      console.log('unzipping', path.resolve(config.drive.mountPoint, 'starter.zip'))
+
+      let zip = new admZip(path.resolve(config.drive.mountPoint, 'starter.zip'))
+
       // TODO: check none of these files already exist before overwrite?
       zip.extractAllTo(config.drive.mountPoint, true)
 
-      if (itemConfig.extractFolderRoot) {
-        let files = []
-        fsextra.walk(path.resolve(config.drive.mountPoint, itemConfig.extractFolderRoot))
-          .on('data', function (file) {
-            files.push(file.path)
-          })
-          .on('end', function () {
-            async.each(files, function (file, done) {
-              fsextra.move(file, file.replace(itemConfig.extractFolderRoot + '/', ''), { mkdirp: true }, function (err) {
-                if (err && err.code === 'EEXIST') {
-                  // ignore
-                } else if (err) {
-                  return done(err)
-                }
-                done()
-              })
-            }, function (err) {
-              if (err) {
-                return console.error(err)
+      let files = []
+      fsextra.walk(path.resolve(config.drive.mountPoint, 'starter'))
+        .on('data', function (file) {
+          files.push(file.path)
+        })
+        .on('end', () => {
+          async.each(files, function (file, done) {
+            fsextra.move(file, file.replace(config.drive.mountPoint + '/', ''), { mkdirp: true }, function (err) {
+              if (err && err.code === 'EEXIST') {
+                // ignore
+              } else if (err) {
+                return done(err)
               }
-              // remove previous dir
-              fsextra.remove(path.resolve(config.drive.mountPoint, itemConfig.extractFolderRoot))
-              fin()
+              done()
+            })
+          }, (err) => {
+            if (err) {
+              return console.error(err)
+            }
+            // remove previous dir
+            fsextra.remove(path.resolve(config.drive.mountPoint, 'starter'))
+
+            this.setState({
+              ...this.state,
+              finished: true
             })
           })
-      } else {
-        fin()
-      }
-
-      const fin = () => {
-        this.setState({
-          ...this.state,
-          copy: {
-            ...this.state.copy,
-            [item]: 100
-          }
         })
-      }
+    })
+  },
+  getText () {
+    if (this.state.downloading === null) {
+      return <div>Loading...</div>
+    } else if (this.state.finished === true) {
+      return <div>Finished...</div>
     } else {
-      let stat = fs.statSync(path.resolve(downloadTo, itemConfig.filename))
-      let p = streamProgress({
-        length: stat.size
-      })
-
-      p.on('progress', (progress) => {
-        this.setState({
-          ...this.state,
-          copy: {
-            ...this.state.copy,
-            [item]: 100
-          }
-        })
-      })
-
-      // TODO: error handling
-      fs.createReadStream(path.resolve(downloadTo, itemConfig.filename))
-        .pipe(p)
-        .pipe(fs.createWriteStream(path.resolve(config.drive.mountPoint, itemConfig.filename)))
+      return <div>Downloading {this.state.downloading} ({this.state.progress}%)</div>
     }
   },
-  startDownload() {
-    items.soundhax.url = `https://github.com/nedwill/soundhax/blob/master/soundhax-${config.region.toLowerCase()}-${config.model}.m4a?raw=true`
-    this.download('soundhax')
-    this.download('hbsk')
-    ipcRenderer.on('otherappURLReply', (event, url) => {
-      items.otherapp = {
-        url,
-        filename: 'otherapp.bin'
-      }
-      this.download('otherapp')
-    })
-    ipcRenderer.send('otherappURL', JSON.stringify(config))
+  getContent () {
+    return <div>
+      <ScaleLoader />
+      {this.getText()}
+    </div>
   },
   render() {
     return (
       <section>
         <div className={section.content}>
-          soundhax
-          <br/>
-          <Circle
-            className={styles.circle}
-            strokeWidth="6"
-            percent={this.getDownloadProgress('soundhax')}
-          />
-          <br/>
-          homebrew starter kit
-          <br/>
-          <Circle
-            className={styles.circle}
-            strokeWidth="6"
-            percent={this.getDownloadProgress('hbsk')}
-          />
-          <br/>
-          otherapp
-          <br/>
-          <Circle
-            className={styles.circle}
-            strokeWidth="6"
-            percent={this.getDownloadProgress('otherapp')}
-          />
+          {this.getContent()}
         </div>
         <div className={section.navigation}>
           <div className={content.button} onClick={browserHistory.goBack}>Back</div>
