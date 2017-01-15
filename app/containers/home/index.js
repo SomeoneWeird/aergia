e8a95b1e60cdcdcb92b0ddc489da81ba7b3d846a
Curia <admin@curiadesign.net>
2017-01-15 18:19:36 +1100

    Fixed some positioning layout

--- app/containers/home/index.js
+++ app/containers/home/index.js
@@ -5,16 +5,14 @@ import { Link } from 'react-router';
 import section from '../cssModules/section.scss';
 import content from '../cssModules/content.scss';
 
-import styles from './index.css'
-
 export default class HomePage extends Component {
   render() {
     return (
-      <section className={styles.section}>
+      <section className={section.intro}>
         <h2 className={section.title}>Aergia</h2>
         <div className={section.content}>
           <p>An interactive guide to A9LH + Luma on your 3DS</p>
-          <Link className={`${content.button} ${styles.button}`} to="/config/model">Start</Link>
+          <Link className={content.button} to="/config/model">Start</Link>
         </div>
       </section>
     );
--- app/containers/sidebar/index.js
+++ app/containers/sidebar/index.js
@@ -16,16 +16,14 @@ function openGithub () {
 export default class sidebar extends Component {
   render() {
     return (
-      <div className={layout.sidebar}>
+      <div className={layout.sidebar + ' col-xs-2'}>
         <div className={layout.sidebarLogo}>
           <span>Aergia</span>
         </div>
-        <div className={layout.sidebarLinks}>
-          <Link to="/credits">Credits</Link>
-          <br />
-          <br />
-          <Link to="#" onClick={openGithub}>Github</Link>
-        </div>
+        <ul className={layout.sidebarLinks}>
+          <li><Link to="/credits">Credits</Link></li>
+          <li><Link to="#" onClick={openGithub}>Github</Link></li>
+        </ul>
       </div>
     )
   }
