6610c0befd545b9b9dfe5430db95d87651195bbd
Adam Brady <adam@boxxen.org>
2017-01-17 22:52:54 +1100

    refactor entrypoints page

--- app/containers/entrypoints/determine/index.js
+++ app/containers/entrypoints/determine/index.js
@@ -8,6 +8,8 @@ import config from '../../../config'
 import section from '../../cssModules/section.scss'
 import content from '../../cssModules/content.scss'
 
+import styles from './index.css'
+
 const entrypoints = {
   n3ds: {
     '>=9.0.0 <=11.2.0': [
@@ -36,38 +38,42 @@ function getEntrypoints () {
   return validEntrypoints
 }
 
-function getEntryLink (name) {
-  return <Link to={{ pathname: '/selectsd', query: { returnTo: `/entry/${name}/start` } }}>{name}</Link>
-}
-
-function otherEntrypoints () {
-  let entrypoints = getEntrypoints()
-  let out
-  if (entrypoints.length > 1) {
-    let points = entrypoints.map(getEntryLink)
-    points.shift()
-    out = <div>
-      You can also use one of the following exploits:
-      {points}
+function getEntryLinks (entrypoints) {
+  return entrypoints.map((name) => {
+    return <div className={styles.entrypointButton}>
+      <Link className={`${content.button}`} to={{ pathname: '/selectsd', query: { returnTo: `/entry/${name}/start` } }}>{name}</Link>
     </div>
-  } else {
-    out = <div></div>
-  }
-  return out
+  })
 }
 
-function getContent () {
+function getSystemEntrypoints () {
   let entrypoints = getEntrypoints()
   if (entrypoints.length) {
     return <div>
-      We recommend you use {getEntryLink(entrypoints[0])} as your exploit
-      {otherEntrypoints()}
+      {getEntryLinks(entrypoints)}
     </div>
   } else {
     return <div>
-      Unfortuntely it looks like there are no exploits for your system+version at the moment!
+      Unfortuntely no system entrypoints are compatible.
+      <br /><br />
+      Do you have any of the below games?
     </div>
   }
+}
+
+function getGameEntrypoints () {
+  return <div>
+    TODO
+  </div>
+}
+
+function getContent () {
+  return <div>
+    <h3>System Entrypoints</h3>
+    {getSystemEntrypoints()}
+    <h3>Game Entrypoints</h3>
+    {getGameEntrypoints()}
+  </div>
 }
 
 export default class determineEntrypoint extends Component {
@@ -75,7 +81,7 @@ export default class determineEntrypoint extends Component {
     return (
       <section>
         <h2 className={section.title}>Select Exploit</h2>
-        <div className={section.content}>
+        <div className={`${section.content} ${styles.center}`}>
           {getContent()}
         </div>
         <div className={section.navigation}>
