15d65ebbdec45620738d71367d8a8eaeb48a2ee7
Adam  Phillips <aphillips@squiz.net>
2017-01-20 10:04:36 +1100

    Set up for material.io components

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Router, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import routes from './routes';
