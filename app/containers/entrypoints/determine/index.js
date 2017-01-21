5b79888220c91de9172b008a04687611c079028f
Adam Brady <adam@boxxen.org>
2017-01-22 00:16:20 +1100

    refactor entrypoint determine screen

--- app/containers/entrypoints/determine/index.js
+++ app/containers/entrypoints/determine/index.js
@@ -28,61 +28,56 @@ const entrypoints = {
   }
 }
 
-function getEntrypoints () {
-  let validEntrypoints = []
-  for (let k in entrypoints[config.model]) {
-    if (semver.satisfies(config.version, k)) {
-      validEntrypoints = validEntrypoints.concat(entrypoints[config.model][k])
+export default class determineEntrypoint extends Component {
+  getEntrypoints () {
+    let validEntrypoints = []
+    for (let k in entrypoints[config.model]) {
+      if (semver.satisfies(config.version, k)) {
+        validEntrypoints = validEntrypoints.concat(entrypoints[config.model][k])
+      }
+    }
+    return validEntrypoints
+  }
+  getEntryLinks (entrypoints) {
+    return entrypoints.map((name) => {
+      return <div key={name} className={styles.entrypointButton}>
+        <Link className={`${content.button}`} to={{ pathname: '/selectsd', query: { returnTo: `/entry/${name}/start` } }}>{name}</Link>
+      </div>
+    })
+  }
+  getSystemEntrypoints () {
+    let entrypoints = this.getEntrypoints()
+    if (entrypoints.length) {
+      return <div>
+        {this.getEntryLinks(entrypoints)}
+      </div>
+    } else {
+      return <div>
+        Unfortuntely no system entrypoints are compatible.
+        <br /><br />
+        Do you have any of the below games?
+      </div>
     }
   }
-  return validEntrypoints
-}
-
-function getEntryLinks (entrypoints) {
-  return entrypoints.map((name) => {
-    return <div key={name} className={styles.entrypointButton}>
-      <Link className={`${content.button}`} to={{ pathname: '/selectsd', query: { returnTo: `/entry/${name}/start` } }}>{name}</Link>
-    </div>
-  })
-}
-
-function getSystemEntrypoints () {
-  let entrypoints = getEntrypoints()
-  if (entrypoints.length) {
+  getGameEntrypoints () {
     return <div>
-      {getEntryLinks(entrypoints)}
-    </div>
-  } else {
-    return <div>
-      Unfortuntely no system entrypoints are compatible.
-      <br /><br />
-      Do you have any of the below games?
+      TODO
     </div>
   }
-}
-
-function getGameEntrypoints () {
-  return <div>
-    TODO
-  </div>
-}
-
-function getContent () {
-  return <div>
-    <h3>System Entrypoints</h3>
-    {getSystemEntrypoints()}
-    <h3>Game Entrypoints</h3>
-    {getGameEntrypoints()}
-  </div>
-}
-
-export default class determineEntrypoint extends Component {
+  getContent () {
+    return <div>
+      <h3>System Entrypoints</h3>
+      {this.getSystemEntrypoints()}
+      <h3>Game Entrypoints</h3>
+      {this.getGameEntrypoints()}
+    </div>
+  }
   render() {
     return (
       <section>
         <h2 className={section.title}>Select Exploit</h2>
         <div className={`${section.content} ${styles.center}`}>
-          {getContent()}
+          {this.getContent()}
         </div>
         <div className={section.navigation}>
           <div className={content.button} onClick={browserHistory.goBack}>Back</div>
