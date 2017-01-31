3d10b98d6e1a8945dc3dcf974359b713cc3c801c
Adam Brady <adam@boxxen.org>
2017-02-01 00:51:04 +1100

    sidemenu

--- app/containers/header/index.js
+++ app/containers/header/index.js
@@ -1,16 +1,53 @@
 import React from 'react'
 import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
 
-function handleTouchTap () {
-  console.log('onTouchTap triggered on the title component')
+import open from 'open'
+
+export default class Header extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      sidebarOpen: false
+    }
+    this.toggleSidebar = this.toggleSidebar.bind(this)
+    this.openGithub = this.openGithub.bind(this)
+  }
+  toggleSidebar () {
+    this.setState({
+      ...this.state,
+      sidebarOpen: !this.state.sidebarOpen
+    })
+  }
+  openGithub () {
+    open('https://github.com/SomeoneWeird/aergia')
+    this.setState({
+      ...this.state,
+      sidebarOpen: false
+    })
+  }
+  render () {
+    return (
+      <div style={{width:"100%"}}>
+        <AppBar
+          title='AERGIA'
+          iconClassNameRight='muidocs-icon-navigation-expand-more'
+          onLeftIconButtonTouchTap={this.toggleSidebar}
+        />
+        <Drawer
+          docked={false}
+          open={this.state.sidebarOpen}
+          onRequestChange={(sidebarOpen) => {
+            this.setState({
+              ...this.state,
+              sidebarOpen
+            })
+          }}
+        >
+          <MenuItem onClick={this.openGithub}>Github</MenuItem>
+        </Drawer>
+      </div>
+    )
+  }
 }
-
-const Header = () => (
-  <AppBar
-    title='AERGIA'
-    iconClassNameRight='muidocs-icon-navigation-expand-more'
-    onLeftIconButtonTouchTap={handleTouchTap}
-  />
-)
-
-export default Header
